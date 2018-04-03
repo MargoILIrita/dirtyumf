09157d457f9e3c4f34809f5b3a1ca27cff60f588
Margarita <rita96pol@gmail.com>
2018-04-03 20:03:07 +0400

    новые формулы

--- lastsemester.py
+++ lastsemester.py
@@ -6,86 +6,83 @@ import mathmethods as mm
 
 def ir(ri):
     if(0 <= ri <= mm.da['a']):
-        return mm.da['P']/(math.pi*mm.da['a']**2)
+        a = mm.da['P']/(math.pi*mm.da['a']**2)
+        return a
     return 0
 
 
-def psi():
-    return mm.da['k']/mm.da['c']
+def psi(ht):
+    return 2 * mm.da['alf'] * ht / (mm.da['c'] * mm.da['l'])
 
 
-def gamma():
-    return 2*mm.da['alf']/(mm.da['c']*mm.da['l'])
-
-
-def fi(ri):
-    return mm.da['betta']*ir(ri)/mm.da['c']
+def gamma(ht, hr):
+    return mm.da['k'] * ht / (mm.da['c'] * hr**2)
 
 
 def p0(ht, hr):
-    return 1 - (4*psi()*ht/hr**2) - gamma()*ht
+    return 1 + 4*gamma(ht,hr) + psi(ht)
 
 
 def q0(ht, hr):
-    return 4*psi()*ht/hr**2
+    return 4*gamma(ht,hr)
 
 
 def s0(w, ht):
-    return w + fi(0)*ht
+    return w + mm.da['betta']*ht*ir(0)/mm.da['c']
 
 
 def ppi(hr, ht):
-    return 1+ 2*psi()*ht/(hr**2) + gamma()*ht
+    return 1+ 2*gamma(ht, hr) + psi(ht)
 
 
 def uui(ht, hr, ri):
-    return ht*psi()/(2*ri*hr) - psi()*ht/hr**2
+    return gamma(ht, hr)*(hr/(2*ri) - 1)
 
 
 def qqi(ht, hr, ri):
-    return psi()*ht/hr**2 + psi()*ht/(2*ri*hr)
+    return gamma(ht,hr)*(hr/(2*ri) + 1)
 
 
 def ssi(w, ri, ht):
-    return w + fi(ri)*ht
+    return w + mm.da['betta']*ht*ir(ri)/mm.da['c']
 
 
-def uI(hr, ht):
-    return 2*psi()*ht/hr**2
+def uI(hr, ht, ri):
+    return 1 + 2*gamma(ht, hr) + psi(ht)
 
 
-def pI(ht, hr):
-    return 1+2*psi()*ht/hr**2 + gamma()*ht
+def pI(ht, hr, ri):
+    return 2*gamma(ht, hr)
 
 
 def sI(w, ri, ht):
-    return w + fi(5)*ht
+    return w + mm.da['betta']*ht*ir(ri)/mm.da['c']
 
 
-def alfa1(ht, hr):
+def alfa0(ht, hr):
     return q0(ht, hr)/p0(ht, hr)
 
 
-def betta1(w, ht, hr):
+def betta0(w, ht, hr):
     return s0(w, ht)/p0(ht, hr)
 
 
 #alfa j
 #ri для j-1, alf для j-1
 def alfaj(q, p, u, al):
-    return q/(p-u*al)
+    return q/(u*al + p)
 
 
 #b k-1 j
 #ri для j-1, alf для j-1, betta для j-1
 def bettaj(s, u, bet, p, al):
-    return (s+u*bet)/(p-u*al)
+    return (s - bet*u)/(u*al + p)
 
 
 #w k I
 #betta for I k-1, alf for I
-def wI(w, ri, ht, hr, bettaI, alf):
-    return (sI(w,ri,ht) - uI(hr,ht)*bettaI)/(pI(ht, hr)-uI(hr, ht)*alf)
+def wI(s,u, p, bet, alf):
+    return (s - u*bet)/(p+u*alf)
 
 
 #w k m
@@ -95,46 +92,26 @@ def wm(alf, bett, w):
 
 
 def straight_run(ht, hr, I, ris, ws):
-    ss = []
-    ps = []
-    qs = []
-    for i in range(I):
-        if i == 0:
-            ss.append(s0(ws[0],ht))
-            ps.append(p0(ht, hr))
-            qs.append(q0(ht,hr))
-        elif i == I-1:
-            ss.append(sI(ws[i], ris[i], ht))
-            ps.append(pI(ht,hr))
-            qs.append(qqi(ht, hr, ris[i]))
-        else:
-            ss.append(ssi(ws[i],ris[i],ht))
-            ps.append(ppi(hr,ht))
-            qs.append(qqi(ht,hr,ris[i]))
-    us = []
     alfas = []
     bettas = []
-    alfas.append(alfa1(ht, hr))
-    bettas.append(betta1(ws[0], ht, hr))
-    us.append(uui(hr,ht,ris[1]))
-    for j in range(1,I,1):
-        i = j-1
-        alfas.append(alfaj(qs[j], ps[j], us[i], alfas[i]))
-        bettas.append(bettaj(ss[j],us[i], bettas[i], ps[j], alfas[i]))
-        us.append(uui(hr, ht, ris[j]))
-    return alfas, bettas
+    alfas.append(alfa0(ht, hr))
+    bettas.append(betta0(ws[0], ht, hr))
+    for j in range(1,I-1,1):
+        alfas.append(alfaj(qqi(ht,hr,ris[j]),ppi(ht,ht),uui(ht,hr,ris[j]),alfas[j-1]))
+        bettas.append(bettaj(ssi(ws[j], ris[j], ht),uui(ht,hr, ris[j]),bettas[j-1],ppi(hr, ht),alfas[j-1]))
+    return alfas, bettas, wI(sI(ws[I-1],ris[I-1], ht),uI(hr, ht,ris[I-1]),pI(ht, hr, ris[I-1]),bettas[I-2],alfas[I-2])
 
 
 def back_stroke(ht, hr, I, ris, ws):
-    alfas, bettas = straight_run(ht, hr, I, ris, ws)
+    alfas, bettas, wi = straight_run(ht, hr, I, ris, ws)
     wnew = []
-    wnew.append(wI(ws[I-1],ris[I-1], ht, hr, bettas[I-1], alfas[I-1]))
+    wnew.append(wi)
     for i in range(I-2, -1, -1):
         wnew.append(wm(alfas[i],bettas[i], wnew[I-i-2]))
     wn = []
     for i in range(I-1,-1,-1):
         wn.append(wnew[i])
-    print(wn)
+   # print(wn)
     return wn
 
 def allinone(ht, hr):
@@ -151,6 +128,13 @@ def allinone(ht, hr):
 
 
 
+a = allinone(1, 0.1)
+b = []
+for t in numpy.arange(100):
+   b.append([mm.u(step, t, 0.01) for step in numpy.arange(0, 5, 0.1)])
 
+for i in range(100):
+    print("new {0}".format(a[i]))
+    print("old {0}".format(b[i]))
 
 

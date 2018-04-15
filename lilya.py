c027760580bf4f94f591436912fd8fba96c87c6d
Margarita <rita96pol@gmail.com>
2018-04-15 20:40:40 +0400

    неправильно(

--- lilya.py
+++ lilya.py
@@ -25,7 +25,7 @@ def p0(ht, hr):
 
 
 def q0(ht, hr):
-    return 4*gamma(ht,hr)
+    return -4*gamma(ht,hr)
 
 
 def s0(w, ht):
@@ -33,15 +33,15 @@ def s0(w, ht):
 
 
 def ppi(hr, ht, ri):
-    return 1+ 2*gamma(ht, hr) + psi(ht)
+    return 1+ 2*gamma(ht, hr) + psi(ht) - gamma(ht, hr)*hr/ri
 
 
 def uui(ht, hr, ri):
-    return ht/(2*hr*ri) - gamma(ht, hr)
+    return gamma(ht, hr) - gamma(ht, hr)*hr/ri
 
 
 def qqi(ht, hr, ri):
-    return gamma(ht,hr) + ht/(2*hr*ri)
+    return -1*gamma(ht,hr)
 
 
 def ssi(w, ri, ht):
@@ -49,7 +49,7 @@ def ssi(w, ri, ht):
 
 
 def uI(hr, ht, ri):
-    return gamma(ht,hr) - gamma(ht, hr) * hr/ri
+    return gamma(ht, hr)*hr/ri - gamma(ht,hr)
 
 
 def pI(ht, hr, ri):
@@ -58,29 +58,6 @@ def pI(ht, hr, ri):
 
 def sI(w, ri, ht):
     return w + mm.da['betta']*ht*ir(ri)/mm.da['c']
-
-
-def alfa0(ht, hr):
-    return q0(ht, hr)/p0(ht, hr)
-
-
-def betta0(w, ht, hr):
-    return s0(w, ht)/p0(ht, hr)
-
-
-#alfa j
-#ri для j-1, alf для j-1
-def alfaj(ht, hr, ri, alfa):
-    return qqi(ht, hr, ri)/( ppi(ht, ht, ri) + uui(ht, hr, ri)* alfa)
-
-
-
-#b k-1 j
-#ri для j-1, alf для j-1, betta для j-1
-def bettaj(w, ri, ht, hr, betta, alfa):
-    u = uui(ht, hr, ri)
-    return (ssi(w, ri, ht) - u* betta)/(ppi(hr, ht, ri) + alfa*u)
-
 
 #w k I
 #betta for I k-1, alf for I
@@ -95,40 +72,5 @@ def wI(w, ri, ht, hr, betta, alfa):
 def wm(alf, bett, w):
     return alf*w + bett
 
-
-def straight_run(ht, hr, I, ris, ws):
-    alfas = []
-    bettas = []
-    alfas.append(alfa0(ht, hr))
-    bettas.append(betta0(ws[0], ht, hr))
-    for j in range(1,I-1,1):
-        alfas.append(alfaj(ht, hr, ris[j], alfas[j-1]))
-        bettas.append(bettaj(ws[j], ris[j], ht, hr, bettas[j-1], alfas[j-1]))
-    return alfas, bettas, wI(ws[I-1],ris[I-1], ht, hr, bettas[I-2], alfas[I-2])
-
-
-def back_stroke(ht, hr, I, ris, ws):
-    alfas, bettas, wi = straight_run(ht, hr, I, ris, ws)
-    wnew = []
-    wnew.append(wi)
-    for i in range(I-2, -1, -1):
-        wnew.append(wm(alfas[i],bettas[i], wnew[I-i-2]))
-    wn = []
-    for i in range(I-1,-1,-1):
-        wn.append(wnew[i])
-   # print(wn)
-    return wn
-
-def allinone(ht, hr, time=100):
-    t = []
-    I = int(mm.da['R']/hr)-1
-    K = int(time/ht)
-    for i in range(I):
-        t.append(0)
-    w = [t,]
-    ris = [x for x in numpy.arange(0,mm.da['R'],hr)]
-    for k in range(1, K, 1):
-        w.append(back_stroke(ht,hr,I, ris,w[k-1]))
-    return w
 
 
